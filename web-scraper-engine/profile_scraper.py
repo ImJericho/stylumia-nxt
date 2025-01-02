6ed79888b8e72beaa649de3ff30ca6d2e5fc35ce
shashvat_sj <20je0897@mc.iitism.ac.in>
2025-01-02 22:21:21 +0530

    connected 3 components

--- web-scraper-engine/profile_scraper.py
+++ web-scraper-engine/profile_scraper.py
@@ -1,13 +1,15 @@
 import instaloader
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+from top100_bs import top100
+import time
 
 
-def extract_instagram_post_urls(profile_name, session_username):
+def extract_instagram_post_urls_for_profile(profile_name, session_username):
     loader = instaloader.Instaloader()
 
     today = datetime.now()
-    one_month_ago = today - relativedelta(months=1)
+    one_month_ago = today - relativedelta(days=3)
     one_month_ago_timestamp = int(one_month_ago.timestamp())
 
     try:
@@ -30,10 +32,29 @@ def extract_instagram_post_urls(profile_name, session_username):
         print(f"An error occurred: {e}")
 
 
-profile_name = "_arshdeep.singh__"
-session_username = "shashvat_sj"
-post_urls = extract_instagram_post_urls(profile_name, session_username)
-if post_urls:
-    print(f"Extracted {len(post_urls)} post URLs from {profile_name}:")
-    for url in post_urls:
-        print(url)
+def profiles_scraper():
+    post_urls_for_all_profiles = []
+    instagram_profile_links = top100()
+    print(instagram_profile_links)
+    session_username = "shashvat_sj"
+    c = 0
+    for profile_name in instagram_profile_links:
+        post_urls_for_one_profile = extract_instagram_post_urls_for_profile(
+            profile_name, session_username
+        )
+        if post_urls_for_one_profile:
+            print(
+                f"Extracted {len(post_urls_for_one_profile)} post URLs from {profile_name}:"
+            )
+            for url in post_urls_for_one_profile:
+                print(url)
+            post_urls_for_all_profiles.extend(post_urls_for_one_profile)
+        time.sleep(30)
+        c += 1
+        if c == 10:
+            break
+
+    return post_urls_for_all_profiles
+
+
+# profiles_scraper()
--- web-scraper-engine/top100_bs.py
+++ web-scraper-engine/top100_bs.py
@@ -4,23 +4,17 @@ from bs4 import BeautifulSoup
 
 def extract_instagram_links(url):
     try:
-        # Send a GET request to the URL
         response = requests.get(url)
-        response.raise_for_status()  # Raise an error for invalid HTTP responses
+        response.raise_for_status()
 
-        # Parse the page content with BeautifulSoup
         soup = BeautifulSoup(response.text, "html.parser")
 
-        # Find all anchor tags containing Instagram links
         links = soup.find_all("a", class_="user-id text-body d-block mb-1")
 
-        # Filter links that contain 'instagram.com'
         # instagram_links = [
         #     link["href"] for link in links if "instagram.com" in link["href"]
         # ]
         instagram_links = [link.get_text(strip=True) for link in links]
-        for i in range(len(instagram_links)):
-            instagram_links[i] = "https://www.instagram.com/" + instagram_links[i]
 
         return instagram_links
     except Exception as e:
@@ -28,13 +22,18 @@ def extract_instagram_links(url):
         return []
 
 
-# URL of the Viral Pitch page
-url = "https://viralpitch.co/topinfluencers/instagram/top-100-instagram-influencers/"
+def top100():
+    url = (
+        "https://viralpitch.co/topinfluencers/instagram/top-100-instagram-influencers/"
+    )
+    instagram_profile_links = extract_instagram_links(url)
 
-# Extract Instagram profile links
-instagram_profile_links = extract_instagram_links(url)
+    print("Instagram Profile Links:")
+    for link in instagram_profile_links:
+        print(link)
+        break
 
-# Print the extracted Instagram links
-print("Instagram Profile Links:")
-for link in instagram_profile_links:
-    print(link)
+    return instagram_profile_links
+
+
+top100()
